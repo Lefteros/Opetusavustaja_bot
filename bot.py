606ba49a7d7fe0039c47f8cd72e59c836dfb8f9e
Tuomas Ruuttunen <tuomasruuttunen@Tuomass-MacBook-Pro.local>
2022-07-25 10:50:30 +0300

    filepath issue maybe fixed

--- bot.py
+++ bot.py
@@ -14,9 +14,9 @@ TOKEN = '5410426109:AAEf5TXFKKG9Evy3p658gBVj0PLc7MeJV7M'
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    with open('/data/tervehdys.txt') as file:
+    with open('data/tervehdys.txt') as file:
         lines = file.readlines()
-    update.message.reply_text('ääliö älä lyö')
+    update.message.reply_text(lines)
 
 def help(update, context):
     """Send a message when the command /help is issued."""
