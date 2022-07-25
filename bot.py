066b3ae64a40c15a14730f1aa63b9a109062e938
Tuomas Ruuttunen <tuomasruuttunen@Tuomass-MacBook-Pro.local>
2022-07-25 10:52:37 +0300

    works, changing formating

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ TOKEN = '5410426109:AAEf5TXFKKG9Evy3p658gBVj0PLc7MeJV7M'
 def start(update, context):
     """Send a message when the command /start is issued."""
     with open('data/tervehdys.txt') as file:
-        lines = file.readlines()
-    update.message.reply_text(lines)
+        content = file.read()
+    update.message.reply_text(content)
 
 def help(update, context):
     """Send a message when the command /help is issued."""
