c40e6761783562ca9ac159577c77088dc4f9284c
Tuomas Ruuttunen <tuomasruuttunen@Tuomass-MacBook-Pro.local>
2022-08-09 10:52:34 +0300

    removed hardcoded apikey and set up environment variable

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '5410426109:AAEf5TXFKKG9Evy3p658gBVj0PLc7MeJV7M'
+TOKEN = os.environ.get("API_KEY")
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
